07cfa521ac45642215e166cd51eb649bc209b7c1
DBenson1017 <devinbenson@gmail.com>
2020-09-03 18:07:41 -0400

    formatter method not functional

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
-}
+
+    static capitalize(sentance){
+        return sentance.charAt(0).toUpperCase() + sentance.slice(1)
+
+    }
+
+    static sanitize(sentance){
+        return sentance.replace(/[^A-Za-z0-9-' ]/g,'')
+    }
+
+    static titleize(sentance){
+       let array =  sentance.split(' ')
+       Formatter.capitalize(array[0])
+
+    }
+
+
+} // end of class 
